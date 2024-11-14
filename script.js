cf68c7ab63d6e87254e0525228460c160f36931b
Manan Manwani <manan.manwani@yahoo.in>
2024-11-14 23:18:52 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let balance = 1000;
 let multiplier = 1.0;
 let gameInterval;
 let isPlaying = false;
+const rocket = document.getElementById('rocket');
 
 function updateDisplay() {
   document.getElementById('balance').innerText = balance.toFixed(2);
@@ -15,6 +16,7 @@ function resetGame() {
   document.getElementById('betButton').disabled = false;
   document.getElementById('cashOutButton').disabled = true;
   document.getElementById('result').innerText = '';
+  rocket.style.transform = 'translate(-50%, 0)';
   updateDisplay();
 }
 
@@ -32,10 +34,14 @@ function placeBet() {
 
   gameInterval = setInterval(() => {
     multiplier += 0.05;
+    const rocketHeight = Math.min(multiplier * 20, 600); // Cap rocket height
+    rocket.style.transform = `translate(-50%, -${rocketHeight}px)`;
     updateDisplay();
 
     if (Math.random() < 0.03) { // 3% chance of crashing
       document.getElementById('result').innerText = 'Crashed! You lost your bet.';
+      rocket.style.transition = 'transform 0.5s ease-out';
+      rocket.style.transform = 'translate(-50%, 600px) rotate(720deg)';
       resetGame();
     }
   }, 100); // Increase multiplier every 100 ms
